70cf0372f8f024a80e41887c03206f457496b240
HarrisonBender <83611772+HarrisonBender@users.noreply.github.com>
2021-08-16 15:00:56 -0500

    Delete mysql directory

--- mysql/select.sql
+++
@@ -1,4 +0,0 @@
-select * from blogs;
-select * from tags;
-select * from blogTags;
-select * from users;
--- mysql/users.sql
+++
@@ -1,8 +0,0 @@
-drop user 'blogapp'@'localhost';
-
-create user 'blogapp'@'localhost'
-identified by 'enort';
-
-grant all privileges
-on blog.*
-to 'blogapp'@'localhost'
